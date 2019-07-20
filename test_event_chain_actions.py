Added the 3 spheres cyclic check which checks logic off cells comparison and sphere comparison when spheres might see each other in the wrong order

test_event_chain_actions.py
@@ -134,7 +134,8 @@ class TestEvent2DCells(TestCase):
         step = Step(sphere1, total_step, v_hat, arr.boundaries)
         arr.perform_total_step(arr.cells[0][0], step, draw)
         draw.array_of_cells_snapshot('After Step (Searching overlap bug)',
-                                     arr, 'After_step', )
+                                     arr, 'After_step')
+        self.assertTrue(arr.legal_configuration())
         assert_list(self, sphere1.center, new_loc_1)
         assert_list(self, sphere2.center, new_loc_2)
 
@@ -160,6 +161,27 @@ class TestEvent2DCells(TestCase):
         assert_list(self, sphere1.center, new_loc_1)
         assert_list(self, sphere2.center, new_loc_2)
 
+    def test_collide_through_cyclic_boundary_3_spheres(self):
+        arr = Event2DCells(1, 3, 3)
+        r = 0.3
+        sphere1 = Sphere((0.99, 2.7), r)
+        sphere2 = Sphere((1.2, 0.4), r)
+        sphere3 = Sphere((1.8, 0.6), r)
+        v_hat = (1, 1)/np.sqrt(2)
+        total_step = 5
+
+        arr.cells[2][0].add_spheres(sphere1)
+        arr.cells[0][1].add_spheres(sphere2)
+        arr.cells[0][1].add_spheres(sphere3)
+        output_dir = 'test_garb/3-spheres-cyclic'
+        if not os.path.isdir(output_dir): os.mkdir(output_dir)
+        draw = View2D(output_dir, arr.boundaries)
+        step = Step(sphere3, total_step, v_hat, arr.boundaries)
+        arr.perform_total_step(arr.cells[0][1], step, draw)
+        draw.array_of_cells_snapshot('After Step (Searching overlap bug)',
+                                     arr, 'After_step', )
+        self.assertTrue(arr.legal_configuration())
+
     def test_generate_spheres_save_pic(self):
         arr = TestEvent2DCells.some_arr()
         cell = arr.all_cells[0]
@@ -173,7 +195,7 @@ class TestEvent2DCells(TestCase):
         arr.perform_total_step(cell, step, draw)
         draw.array_of_cells_snapshot('After Step (Searching overlap bug)',
                                      arr, 'After_step', step)
-        pass
+        self.assertTrue(arr.legal_configuration())
 
     def test_generate_spheres_many_times_perform_large_step(self):
         for i in range(100):

test_structure.py
@@ -1,6 +1,5 @@
 from unittest import TestCase
 from Structure import *
-from EventChainActions import Step
 import numpy as np
 from SnapShot import View2D
 
@@ -45,15 +44,28 @@ class TestSphere(TestCase):
         boundaries = CubeBoundaries([1, 1], [BoundaryType.CYCLIC for _ in range(2)])
         sphere = Sphere((0.5, 0.5), 0.3)
         v_hat = np.array((1, 1))/np.sqrt(2)
-        sphere.perform_step(Step(sphere, 9, v_hat, boundaries, 0.6*np.sqrt(2)))
+        sphere.perform_step(v_hat, 0.6*np.sqrt(2), boundaries)
         self.assertAlmostEqual(sphere.center[0], 0.1)
         self.assertAlmostEqual(sphere.center[1], 0.1)
 
     def test_systems_length_in_v_direction(self):
         boundaries = CubeBoundaries([1, 1], [BoundaryType.CYCLIC for _ in range(2)])
         sphere = Sphere((0.5, 0.5), 0.3)
-        v_hat = np.array((1, 1))/np.sqrt(2)
-        self.assertAlmostEqual(sphere.systems_length_in_v_direction(v_hat, boundaries), np.sqrt(2))
+        v_hat = (1, 1)/np.sqrt(2)
+        self.assertAlmostEqual(sphere.systems_length_in_v_direction(v_hat, boundaries), \
+                               np.sqrt(2))
+
+        boundaries = CubeBoundaries([2, 1], [BoundaryType.CYCLIC for _ in range(2)])
+        sphere = Sphere((0.5, 0.3), 0.3)
+        v_hat = (1, 1) / np.sqrt(2)
+        self.assertAlmostEqual(sphere.systems_length_in_v_direction(v_hat, boundaries), \
+                               np.sqrt(2))
+
+        boundaries = CubeBoundaries([1, 2], [BoundaryType.CYCLIC for _ in range(2)])
+        sphere = Sphere((0.5, 0.3), 0.3)
+        v_hat = (1, 1) / np.sqrt(2)
+        self.assertAlmostEqual(sphere.systems_length_in_v_direction(v_hat, boundaries), \
+                               0.8*np.sqrt(2))
 
     def test_trajectory(self):
         boundaries = CubeBoundaries([1, 1], [BoundaryType.CYCLIC for _ in range(2)])
@@ -64,16 +76,21 @@ class TestSphere(TestCase):
         self.assertAlmostEqual(new_loc[1], 0.5)
 
     def test_trajectories_braked_to_lines(self):
-        boundaries = CubeBoundaries([1, 1], [BoundaryType.CYCLIC for _ in range(2)])
+        boundaries = CubeBoundaries([1, 1], 2*[BoundaryType.CYCLIC])
         sphere = Sphere((0.5, 0.5), 0.3)
         v_hat = np.array((2, 1))/np.sqrt(5)
+        dt = np.sqrt(5)/4
         ps, ts = sphere.trajectories_braked_to_lines(1.2, v_hat, boundaries)
         self.assertAlmostEqual(np.linalg.norm(ps[0] - (0.5, 0.5)), 0)
         self.assertAlmostEqual(ts[0], 0)
         self.assertAlmostEqual(np.linalg.norm(ps[1] - (0, 0.75)), 0, 3)
-        self.assertAlmostEqual(ts[1], np.linalg.norm(np.array([1, 0.75]) - (0.5, 0.5)), 3)
-        self.assertAlmostEqual(np.linalg.norm(ps[2] - [0.57331263, 0.03665631]), 0, 3)
-        self.assertAlmostEqual(ts[2], 1.2)
+        self.assertAlmostEqual(ts[1], dt, 3)
+        self.assertAlmostEqual(np.linalg.norm(ps[2] - [0.5, 0]), 0, 3)
+        self.assertAlmostEqual(ts[2], 2*dt, 3)
+        fin = 1.2 - 2*dt
+        p_fin = (0.5, 0) + v_hat*fin
+        self.assertAlmostEqual(np.linalg.norm(ps[3] - p_fin), 0, 3)
+        self.assertAlmostEqual(ts[3], 1.2, 3)
 
 
 class TestCubeBoundaries(TestCase):
@@ -279,6 +296,11 @@ class TestArrayOfCells(TestCase):
                 draw = View2D('test_garb', bound)
                 draw.array_of_cells_snapshot('Test overlap 2 cells', arr, 'Test_overlap_2_cells')
                 break
+
+        cell = Cell((0, 0), [1, 1], (0, 0), [Sphere((0.1, 0.5), 0.2)])
+        neighbor = Cell((0, 1), [1, 1], (0, 0), [Sphere((0.1, 1.5), 0.2)])
+        arr = ArrayOfCells(2, CubeBoundaries([2, 1], 2*[BoundaryType.CYCLIC]), [cell, neighbor])
+        self.assertTrue(arr.overlap_2_cells(cell, neighbor))
 
     def test_cushioning_array_for_boundary_cond(self):
         arr, _, _ = TestArrayOfCells.construct_some_arr_cell()